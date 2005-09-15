5c7667708cc6f11e719da7e8571a542617c2d41f
Malte Sussdorff <openacs@sussdorff.de>
2005-09-15 11:11:27 +0000

    Reverting bad commit

--- www/invoice-ae.tcl
+++ www/invoice-ae.tcl
@@ -6,7 +6,7 @@ ad_page_contract {
 } {
     invoice_id:integer,optional
     {organization_id:integer,optional ""}
-    {project_id:multiple}
+    {project_id:multiple,optional ""}
     {offer_item_ids:array,optional}
     {__new_p 0}
     {mode edit}

e86cc654daea50b207c8e17d2b6341709a299cfa
Malte Sussdorff <openacs@sussdorff.de>
2006-04-03 11:44:22 +0000

    Added error to invoice-search

--- www/invoice-search.tcl
+++ www/invoice-search.tcl
@@ -5,6 +5,7 @@ ad_page_contract {
     @creation-date 2005-06-06
 } {
     invoice_nr
+    {return_url ""}
 }
 
 set match_invoices [db_list get_invoices {}]
@@ -12,7 +13,8 @@ set match_length [llength $match_invoices]
 
 if { [string equal $match_length 0] } {
     # No Match just redirect
-    ad_returnredirect $return_url
+    # ad_returnredirect $return_url
+    ad_return_error "[_ invoices.no_such_invoice_no]" "[_ invoices.lt_no_such_invoice_no]"
 
 } else {
     set invoice_id [lindex [lindex $match_invoices 0] 0]

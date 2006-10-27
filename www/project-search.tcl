34ce740d5d156a0bfd4537ccf0370fb7e9da9f0a
Malte Sussdorff <openacs@sussdorff.de>
2006-10-27 16:50:07 +0000

    Changed project-search to use pm::project::search_url

--- www/project-search.tcl
+++ www/project-search.tcl
@@ -5,24 +5,13 @@ ad_page_contract {
     @creation-date 2005-06-06
 } {
     project_id
+    {return_url "/invoices"}
 }
 
-set match_projects [db_list_of_lists get_projects { }]
-set match_length [llength $match_projects]
+set pm_url  "[pm::project::search_url -keyword $project_id]"
 
-if { [string equal $match_length 0] } {
-    # No Match just redirect
-    ad_returnredirect $return_url
-
-} else {
-    set project_item_id [lindex [lindex $match_projects 0] 0]
-    set object_package_id [lindex [lindex $match_projects 0] 2]
-	
-    # We get the node_id from the package_id and use it 
-    # to get the url of the project-manager
-    set pm_node_id [site_node::get_node_id_from_object_id -object_id $object_package_id]
-    set pm_url [site_node::get_url -node_id $pm_node_id]
-    
-    # Just redirect to the pm_url and project_item_id
-    ad_returnredirect "${pm_url}one?project_item_id=$project_item_id"
+if {$pm_url eq ""} {
+    set pm_url $return_url
 }
+ad_returnredirect $pm_url
+
